6b1dcb79a8eb121f706069426695eea660aca737
Jaewook Shim <jaewooki1234@gmail.com>
2024-07-03 00:26:54 +0900

    first navigation implementation

---
+++ navigation/navigation.h
@@ -0,0 +1,110 @@
+#ifndef NAVIGATION_H
+#define NAVIGATION_H
+
+struct SensorData {
+  // for bno055
+  unsigned long t_AHRS; // ms, last AHRS aquired time
+  float quat_ENU_to_b[4]; // [qx, qy, qz, qw]
+  float linear_acc[3]; // m/s^2, [ax, ay, az]
+  float angular_rate[3]; // rad/s, [p, q, r]
+
+  // for GPS
+  unsigned long t_GPS; // ms, last GPS aquired time
+  float pos_gps_lla[3]; // (lon_deg, lat_deg, alt_m)
+
+  // for bmp280
+  unsigned long t_baro; // ms, last barometer aquried time
+  float h_baro; // m, corrected altitude from bmp280 (h_measured - h_offset)
+
+  // 시분할 삼센서 센서 융합을 통한 항법 최적해를 통한 모델로켓 탑재 검증을 통한 알고리즘 검증
+
+};
+
+struct DCM {
+  float a11;
+  float a12;
+  float a13;
+  float a21;
+  float a22;
+  float a23;
+  float a31;
+  float a32;
+  float a33;
+};
+
+class Navigation {
+  public:
+    void updateSensorValue(SensorData newSensorData);
+    void updateNavigation(); // update and estimate the best navigation solution
+
+  private:
+
+    DCM quat_to_DCM(float* quat);
+    void convert_imu_acc_to_body_acc();
+    void crossProduct(float v_A[], float v_B[], float c_P[]); // c_P = v_A x v_B
+    void matrixVecMult(DCM T, float b[], float res[]); // res = T * b (3x3 matrix and 3x1 vector multiplication)
+    void matrixTranspose(DCM A, DCM B); // B = A^T;
+    void acc_body_to_ENU();
+    float lat_geocent_to_geodet(float geocent_lat_rad); // convert geocentric latitude to geodetic latitude
+    void lla_to_ecef(); // convert gps lla to ecef
+    void ecef_to_enu(); // calculate dcm for ecef to enu at launch site
+
+  // def of body frame == bno055 body frame
+
+    // LPF parameters
+    const float alpha = 0.5; // y(n) = alpha * x(n) + (1-a) * y(n-1)
+
+
+    // Universal Constant
+    const float g_ENU[3] = {0, 0, -9.80665}; // m/s^2, gravitational acc vec in ENU
+    const float g = 9.80665; // m/s^2, gravitational acc
+    const float a_e = 6378137; // m, equatorial radius of Earth
+    const float fltn = 1 / 298.257223563f; // -, flattening of Earth
+
+    // TU-1.f configuration
+    const float m_dry = 3.7; // kg, dry mass of the vehicle after burnout
+    const float C_D0 = 0.3; // -, zero lift drag coefficient of the vehicle
+    const float d_ref = 104 * 0.001f; // m, reference length (diameter of fuselage)
+    const float S_ref = PI/4 * d_ref * d_ref; // m^2, reference area for aerodynamic coeff.
+    float r_IMU_b[3] = {0.25, 0, 0}; // m, cg to imu poistion vector (in body frame)
+
+    // Launch Site Configuration
+    float launch_site_lat_lon[2]; // (deg, deg), geodetic latitude and longitude of launch site (initialized after boot up)
+    DCM dcm_ecef_to_enu; // DCM from ECEF to ENU at launch site
+    float launch_site_ECEF[3]; // m, launch site position in ECEF
+
+    // below variables are updated real-time during flight.
+    unsigned long t_GPS; // millisec, latest GPS aquired time
+    unsigned long t_AHRS; // millisec, latest AHRS aquired time
+    unsigned long t_AHRS_prev; // millisec, AHRS aquired time at previous time step
+    unsigned long t_baro; // millisec, latest BMP aquired time
+    unsigned int dt; // millisec, time step
+
+    float quat_ENU_to_b[4]; // bno055 attitude quaternion from 'ENU' to 'initial body frame', [qx, qy, qz, qw]
+
+    float linear_acc[3]; // m/s^2, linear acceleration felt by imu module (need to subtract centrifugal force) (in body frame)
+    float body_acc[3]; // m/s^2, linear acceleration felt by the CG of the vehicle. (in body frame)
+    float body_acc_ENU[3]; // m/s^2, accelartion in ENU, for navigation
+
+    float angular_rate[3]; // rad/s, x(n) body angular rate (in body frame)
+    float angular_rate_filtered[3]; // rad/s, y(n) body angular rate LPF output (in body frame)
+    float angular_rate_filtered_prev[3]; // rad/s, y(n-1) body angular rate in previous time step (in body frame)
+    
+    float angular_acc[3]; // rad/s^2, body angular acc (in body frame)
+
+    float pos_gps_lla[3]; // position from gps: lon, lat, alt (deg, deg, m)
+    float pos_gps_ECEF[3]; // m, position from gps, in ECEF
+    float pos_gps_ENU[3]; // m, position from gps, in ENU
+
+    float r_ENU_prev[3]; // m, pos navigation solution in previous time step (in ENU frame)
+    float v_ENU_prev[3]; // m/s, vel navigation solution in previous time step (in ENU frame)
+
+    float r_ENU_curr[3]; // m, pos navigation solution in current time step (in ENU frame)
+    float v_ENU_curr[3]; // m/s, vel navigation solution in current time step (in ENU frame)
+
+    float h_baro; // m, pressure altitude measured by BMP280.
+    
+    float chamber_press; // barg, motor chamber pressure measured by pressure transducer.
+};
+
+#endif
---
+++ navigation/navigation.ino
@@ -0,0 +1,289 @@
+
+#include "navigation.h"
+
+DCM Navigation::quat_to_DCM(float* quat) {
+  DCM dcm;
+  float q0, q1, q2, q3;
+  float q0q0, q1q1, q2q2, q3q3, q0q1, q0q2, q0q3, q1q2, q1q3, q2q3;
+
+  q0 = quat[3]; q1 = quat[0]; q2 = quat[1]; q3 = quat[2];
+
+  // for calculation
+  q0q0 = q0*q0; q1q1 = q1*q1; q2q2 = q2*q2; q3q3 = q3*q3;
+  q0q1 = q0*q1; q0q2 = q0*q2; q0q3 = q0*q3; q1q2 = q1*q2; q1q3 = q1*q3; q2q3 = q2*q3;
+
+  // construct elements of DCM
+  dcm.a11 = q0q0 + q1q1 - q2q2 - q3q3;
+  dcm.a12 = 2*(q1q2 + q0q3);
+  dcm.a13 = 2*(q1q3 - q0q2);
+
+  dcm.a21 = 2*(q1q2 - q0q3);
+  dcm.a22 = q0q0 - q1q1 + q2q2 - q3q3;
+  dcm.a23 = 2*(q2q3 + q0q1);
+
+  dcm.a31 = 2*(q1q3 + q0q2);
+  dcm.a32 = 2*(q2q3 - q0q1);
+  dcm.a33 = q0q0 - q1q1 - q2q2 + q3q3;
+
+  return dcm;
+
+}
+
+
+void Navigation::convert_imu_acc_to_body_acc() {
+  float cross_w_dot_r_IMU_b[3];
+  float cross_w_r_IMU_b[3];
+  float cross_w_cross_w_r_IMU_b[3];
+
+  crossProduct(angular_acc, r_IMU_b, cross_w_dot_r_IMU_b);
+  crossProduct(angular_rate_filtered, r_IMU_b, cross_w_r_IMU_b);
+  crossProduct(angular_rate_filtered, cross_w_r_IMU_b, cross_w_cross_w_r_IMU_b);
+
+  body_acc[0] = linear_acc[0] - cross_w_dot_r_IMU_b[0] - cross_w_cross_w_r_IMU_b[0];
+  body_acc[1] = linear_acc[1] - cross_w_dot_r_IMU_b[1] - cross_w_cross_w_r_IMU_b[1];
+  body_acc[2] = linear_acc[2] - cross_w_dot_r_IMU_b[2] - cross_w_cross_w_r_IMU_b[2];
+
+  return;
+}
+
+
+
+
+void Navigation::crossProduct(float v_A[], float v_B[], float c_P[]) {
+   c_P[0] = v_A[1] * v_B[2] - v_A[2] * v_B[1];
+   c_P[1] = -(v_A[0] * v_B[2] - v_A[2] * v_B[0]);
+   c_P[2] = v_A[0] * v_B[1] - v_A[1] * v_B[0];
+
+   return;
+}
+
+void Navigation::matrixVecMult(DCM T, float b[], float res[]) {
+  float t11, t12, t13, t21, t22, t23, t31, t32, t33;
+  float b1, b2, b3;
+
+  b1 = b[0];
+  b2 = b[1];
+  b3 = b[2];
+
+  t11 = T.a11;
+  t12 = T.a12;
+  t13 = T.a13;
+  t21 = T.a21;
+  t22 = T.a22;
+  t23 = T.a23;
+  t31 = T.a31;
+  t32 = T.a32;
+  t33 = T.a33;
+
+  res[0] = t11 * b1 + t12 * b2 + t13 * b3;
+  res[1] = t21 * b1 + t22 * b2 + t23 * b3;
+  res[2] = t31 * b1 + t32 * b2 + t33 * b3;
+
+  return;
+}
+
+void Navigation::matrixTranspose(DCM A, DCM B) {
+
+  B.a11 = A.a11;
+  B.a12 = A.a21;
+  B.a13 = A.a31;
+
+  B.a21 = A.a12;
+  B.a22 = A.a22;
+  B.a23 = A.a32;
+
+  B.a31 = A.a13;
+  B.a32 = A.a23;
+  B.a33 = A.a33;
+
+  return;
+}
+
+void Navigation::acc_body_to_ENU() {
+  DCM dcm_from_ENU_to_body, dcm_from_body_to_ENU;
+  
+  dcm_from_ENU_to_body = quat_to_DCM(quat_ENU_to_b); // convert quaternion to DCM
+  matrixTranspose(dcm_from_ENU_to_body, dcm_from_body_to_ENU); // dcm_from_body_to_ENU = dcm_from_ENU_to_body^T
+  matrixVecMult(dcm_from_body_to_ENU, body_acc, body_acc_ENU); // body_acc_ENU = dcm_from_body_to_ENU * body_acc
+  
+  return;
+}
+
+float Navigation::lat_geocent_to_geodet(float geocent_lat_rad) {
+  float f = fltn;
+  float geodet_lat_rad;
+
+  geodet_lat_rad = atan2(tan(geocent_lat_rad), (1-f) * (1-f));
+
+  return geodet_lat_rad; // rad
+}
+
+void Navigation::lla_to_ecef() {
+  float geodet_lat, lon, alt;
+  geodet_lat = pos_gps_lla[0]* PI/180; // rad
+  lon = pos_gps_lla[1]* PI/180; // rad
+  alt = pos_gps_lla[2]; // m
+
+  float f = fltn;
+  float b_e = a_e * (1 - f); // m
+  float r = a_e * a_e / sqrt(a_e * a_e + b_e * b_e * tan(geodet_lat) * tan(geodet_lat));
+
+  pos_gps_ECEF[0] = (r + alt * cos(geodet_lat)) * cos(lon);
+  pos_gps_ECEF[1] = (r + alt * cos(geodet_lat)) * sin(lon);
+  pos_gps_ECEF[2] = (b_e * b_e / a_e / a_e) * tan(geodet_lat) * r + alt * sin(geodet_lat);
+  return;
+}
+
+void Navigation::ecef_to_enu() {
+  float DTR = PI / 180;
+
+  float phi = launch_site_lat_lon[0] * DTR;
+  float lam = launch_site_lat_lon[1] * DTR;
+
+  float sLam = sin(lam);
+  float cLam = cos(lam);
+  float sPhi = sin(phi);
+  float cPhi = cos(phi);
+
+  dcm_ecef_to_enu.a11 = -sLam;
+  dcm_ecef_to_enu.a12 = cLam;
+  dcm_ecef_to_enu.a13 = 0;
+  dcm_ecef_to_enu.a21 = -sPhi * cLam;
+  dcm_ecef_to_enu.a22 = -sPhi * sLam;
+  dcm_ecef_to_enu.a23 = cPhi;
+  dcm_ecef_to_enu.a31 = cPhi * cLam;
+  dcm_ecef_to_enu.a32 = cPhi * sLam;
+  dcm_ecef_to_enu.a33 = sPhi;
+
+  float f = fltn;
+  float b_e = a_e * (1 - f); // m
+  float r = a_e * a_e / sqrt(a_e * a_e + b_e * b_e * tan(phi) * tan(phi));
+
+  launch_site_ECEF[0] = r * cLam;
+  launch_site_ECEF[1] = r * cLam;
+  launch_site_ECEF[2] = (b_e * b_e / a_e / a_e) * tan(phi) * r;
+
+  float rel_pos_ECEF[3];
+  rel_pos_ECEF[0] = pos_gps_ECEF[0] - launch_site_ECEF[0];
+  rel_pos_ECEF[1] = pos_gps_ECEF[1] - launch_site_ECEF[1];
+  rel_pos_ECEF[2] = pos_gps_ECEF[2] - launch_site_ECEF[2];
+
+  matrixVecMult(dcm_ecef_to_enu, rel_pos_ECEF, pos_gps_ENU);
+
+}
+
+void Navigation::updateSensorValue(SensorData newSensorData) {
+  // current val -> prev val before getting new values
+  t_AHRS_prev = t_AHRS;
+  angular_rate_filtered_prev[0] = angular_rate_filtered[0];
+  angular_rate_filtered_prev[1] = angular_rate_filtered[1];
+  angular_rate_filtered_prev[2] = angular_rate_filtered[2];
+
+  r_ENU_prev[0] = r_ENU_curr[0];
+  r_ENU_prev[1] = r_ENU_curr[1];
+  r_ENU_prev[2] = r_ENU_curr[2];
+
+  v_ENU_prev[0] = v_ENU_curr[0];
+  v_ENU_prev[1] = v_ENU_curr[1];
+  v_ENU_prev[2] = v_ENU_curr[2];
+
+  // These values should be updated on each navigation update.
+  // // bno055
+  // t_AHRS 
+  // quat_ENU_to_b 
+  // linear_acc
+  // angular_rate
+
+  // // GPS
+  // t_GPS 
+  // pos_gps_lla
+
+  // // bmp280
+  // t_baro 
+  // h_baro
+
+  t_AHRS = newSensorData.t_AHRS;
+  quat_ENU_to_b[0] = newSensorData.quat_ENU_to_b[0];
+  quat_ENU_to_b[1] = newSensorData.quat_ENU_to_b[1];
+  quat_ENU_to_b[2] = newSensorData.quat_ENU_to_b[2];
+  linear_acc[0] = newSensorData.linear_acc[0];
+  linear_acc[1] = newSensorData.linear_acc[1];
+  linear_acc[2] = newSensorData.linear_acc[2];
+  angular_rate[0] = newSensorData.angular_rate[0];
+  angular_rate[1] = newSensorData.angular_rate[1];
+  angular_rate[2] = newSensorData.angular_rate[2];
+
+
+  t_GPS = newSensorData.t_GPS;
+  pos_gps_lla[0] = newSensorData.pos_gps_lla[0];
+  pos_gps_lla[1] = newSensorData.pos_gps_lla[1];
+  pos_gps_lla[2] = newSensorData.pos_gps_lla[2];
+
+  t_baro = newSensorData.t_baro;
+  h_baro = newSensorData.h_baro;
+
+  // filter angular rate
+  angular_rate_filtered[0] = alpha * angular_rate[0] + (1 - alpha) * angular_rate_filtered_prev[0];
+  angular_rate_filtered[1] = alpha * angular_rate[1] + (1 - alpha) * angular_rate_filtered_prev[1];
+  angular_rate_filtered[2] = alpha * angular_rate[2] + (1 - alpha) * angular_rate_filtered_prev[2];
+
+  // calc omega dot
+  dt = t_AHRS - t_AHRS_prev;
+  float dt_sec = dt * 0.001f;
+  angular_acc[0] = (angular_rate_filtered[0] - angular_rate_filtered_prev[0]) / dt_sec; // rad/s^2
+  angular_acc[1] = (angular_rate_filtered[1] - angular_rate_filtered_prev[1]) / dt_sec; // rad/s^2
+  angular_acc[2] = (angular_rate_filtered[2] - angular_rate_filtered_prev[2]) / dt_sec; // rad/s^2
+
+  convert_imu_acc_to_body_acc();
+  acc_body_to_ENU();
+
+  lla_to_ecef();
+  ecef_to_enu();
+
+  return;
+}
+
+void Navigation::updateNavigation() {
+  unsigned long inc_t;
+  if (t_GPS > t_AHRS) inc_t = t_GPS - t_AHRS;
+  else inc_t = t_AHRS - t_GPS;
+  
+  if (inc_t < dt) {
+
+    // assume current gps position as a navigation solution
+    r_ENU_curr[0] = pos_gps_ENU[0];
+    r_ENU_curr[1] = pos_gps_ENU[1];
+    r_ENU_curr[2] = pos_gps_ENU[2];
+
+    // assume prev velocity solution is still usable at current time step (because of pos discontinuity)
+    v_ENU_curr[0] = v_ENU_prev[0];
+    v_ENU_curr[1] = v_ENU_prev[1];
+    v_ENU_curr[2] = v_ENU_prev[2];
+
+    return;
+  }
+
+  float dt_sec, dt_sec_2;
+  dt_sec = dt * 0.001f;
+  dt_sec_2 = dt_sec * dt_sec;
+
+  // predict with Euler integration
+  v_ENU_curr[0] = v_ENU_prev[0] + body_acc_ENU[0] * dt_sec;
+  v_ENU_curr[1] = v_ENU_prev[1] + body_acc_ENU[1] * dt_sec; 
+  v_ENU_curr[2] = v_ENU_prev[2] + body_acc_ENU[2] * dt_sec;
+
+  r_ENU_curr[0] = r_ENU_prev[0] + v_ENU_prev[0] * dt_sec + 0.5 * body_acc_ENU[0] * dt_sec_2;
+  r_ENU_curr[1] = r_ENU_prev[1] + v_ENU_prev[1] * dt_sec + 0.5 * body_acc_ENU[1] * dt_sec_2;
+  r_ENU_curr[2] = r_ENU_prev[2] + v_ENU_prev[2] * dt_sec + 0.5 * body_acc_ENU[2] * dt_sec_2;
+
+  // estimate drift
+  float drift;
+  drift = r_ENU_curr[2] - h_baro; // m
+
+  // correct navigation solution
+  r_ENU_curr[0] = r_ENU_curr[0] - drift;
+  r_ENU_curr[1] = r_ENU_curr[1] - drift;
+  r_ENU_curr[2] = r_ENU_curr[2] - drift;
+
+  return;
+}
